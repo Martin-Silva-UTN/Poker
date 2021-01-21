57ab0db3c1ce8442ce7554a389f022901d20b037
Martin <martin.silva@alumnos.frm.utn.edu.ar>
2021-01-20 21:23:57 -0300

    Test

---
+++ main.py
@@ -0,0 +1,51 @@
+import arcade
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Starting Template"
+
+
+class MyGame(arcade.Window):
+    """
+    Main application class.
+
+    NOTE: Go ahead and delete the methods you don't need.
+    If you do need a method, delete the 'pass' and replace it
+    with your own code. Don't leave 'pass' in this program.
+    """
+
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+
+        arcade.set_background_color(arcade.color.AMAZON)
+
+        # If you have sprite lists, you should create them here,
+        # and set them to None
+
+    def setup(self):
+        """ Set up the game variables. Call to re-start the game. """
+        # Create your sprites and sprite lists here
+        print('hola')
+
+    def on_draw(self):
+        """
+        Render the screen.
+        """
+
+        # This command should happen before we start drawing. It will clear
+        # the screen to the background color, and erase what we drew last frame.
+        arcade.start_render()
+
+        # Call draw() on all your sprite lists below
+
+
+def main():
+    """ Main method """
+    game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, 'Hola Pochoclita')
+    game.setup()
+
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
